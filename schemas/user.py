2cdee5456a22eb1ecbc246260db563c7680dc81b
unknown <m.m.abdelsalam05@gmail.com>
2023-04-25 14:50:12 +0200

    flask masrhmallow course 5 (fixed problem)

--- schemas/user.py
+++ schemas/user.py
@@ -2,8 +2,9 @@ from models.user import UserModel
 from ma import marsh
 
 
-class UserSchema(marsh.SQLAlchemySchema):
+class UserSchema(marsh.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel
+        load_instance = True
         load_only = ("password",)
         dump_only = ("id",)
