8b9ac370bc200f690696626475ef8c0e6fd1dd93
unknown <m.m.abdelsalam05@gmail.com>
2023-04-26 16:09:13 +0200

    flask uploads course 4

---
+++ resources/image.py
@@ -0,0 +1,27 @@
+from libs import image_helper
+from flask_restful import request, Resource
+from flask import send_file
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_uploads import UploadNotAllowed
+
+from schemas.image import ImageSchema
+
+image_schema = ImageSchema()
+
+
+class UploadImage(Resource):
+    @classmethod
+    @jwt_required
+    def post(cls):
+        data = image_schema.load(request.files)
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"
+        try:
+            image_path = image_helper.save_image(data["image"], folder)
+            basename = image_helper.get_basename(image_path)
+            return {"message": f'image "{basename}" is uploaded successfully'}, 201
+        except UploadNotAllowed:
+            extension = image_helper.get_extension(data["image"])
+            return {
+                "message": f"image extension '{extension}' is not allowed please try another image"
+            }, 400
