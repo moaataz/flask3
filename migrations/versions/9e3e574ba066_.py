c44562ed23fc3899cdcd6518f44be5df621e686e
unknown <m.m.abdelsalam05@gmail.com>
2023-05-01 17:58:47 +0300

    flask migration course 8

--- migrations/versions/9e3e574ba066_.py
+++ migrations/versions/9e3e574ba066_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: af5a92f6cdaf
+Revision ID: 9e3e574ba066
 Revises: 
-Create Date: 2023-05-01 16:39:48.236400
+Create Date: 2023-05-01 17:50:18.391499
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'af5a92f6cdaf'
+revision = '9e3e574ba066'
 down_revision = None
 branch_labels = None
 depends_on = None
