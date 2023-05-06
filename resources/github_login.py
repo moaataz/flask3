ba0dec518b211a6806c74fcb757277c28907104a
unknown <m.m.abdelsalam05@gmail.com>
2023-05-06 21:09:12 +0300

    flask oauth course 10

--- resources/github_login.py
+++ resources/github_login.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from oa import github
-from flask import g, request
+from flask import g, request, url_for
 from models.user import UserModel
 from flask_jwt_extended import create_access_token, create_refresh_token
 
@@ -8,9 +8,7 @@ from flask_jwt_extended import create_access_token, create_refresh_token
 class GithubLogin(Resource):
     @classmethod
     def get(cls):
-        return github.authorize(
-            callback="http://localhost:5000/login/github/authorized"
-        )
+        return github.authorize(callback=url_for("github.authorize", _external=True))
 
 
 class GithubAuthorize(Resource):
