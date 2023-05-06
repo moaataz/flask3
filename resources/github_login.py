6c2f11e28a84aae48d0d9769a04f551e64311b28
unknown <m.m.abdelsalam05@gmail.com>
2023-05-06 19:54:31 +0300

    flask oauth course 8

--- resources/github_login.py
+++ resources/github_login.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from oa import github
-from flask import g
+from flask import g, request
 from models.user import UserModel
 from flask_jwt_extended import create_access_token, create_refresh_token
 
@@ -17,6 +17,13 @@ class GithubAuthorize(Resource):
     @classmethod
     def get(cls):
         resp = github.authorized_response()
+        if resp is None or resp.get("access_token") is None:
+            error_response = {
+                "error": request.args["error"],
+                "error_description": request.args["error_description"],
+            }
+            return error_response
+
         g.access_token = resp["access_token"]
         github_user = github.get("user")
         github_username = github_user.data["login"]
